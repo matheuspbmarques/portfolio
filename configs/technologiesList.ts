8385e03d4806d973f4a8e0d93430085f0fafbe3d
Matheus Marques <matheuspbmarques@protonmail.com>
2025-03-13 16:51:29 -0300

    add: heroku

--- configs/technologiesList.ts
+++ configs/technologiesList.ts
@@ -1,6 +1,6 @@
 type LevelsTypes = 'basic' | 'intermedium' | 'advanced'
 
-const technologiesList:Array<String> = ['HTML', 'CSS', 'JavaScript', 'Node', 'Express', 'React', "Next", 'Tailwind', 'TypeScript', 'Git', 'Nest', 'Docker', 'PHP', 'Jest', 'Laravel', 'MySQL', 'PostgreSQL', 'MariaDB', 'OracleDB', 'MongoDB', 'React Native', 'AWS', 'Prisma', "CI/CD"]
+const technologiesList:Array<String> = ['HTML', 'CSS', 'JavaScript', 'Node', 'Express', 'React', "Next", 'Tailwind', 'TypeScript', 'Git', 'Nest', 'Docker', 'PHP', 'Jest', 'Laravel', 'MySQL', 'PostgreSQL', 'MariaDB', 'OracleDB', 'MongoDB', 'React Native', 'AWS', 'Prisma', "CI/CD", "Heroku"]
 
 export default technologiesList
 export type { LevelsTypes }
