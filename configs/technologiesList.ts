e3b4331bfa41b03a5a294adc4193bee208526ef4
Matheus Marques <matheuspbmarques@protonmail.com>
2025-03-13 16:52:14 -0300

    add: GCP

--- configs/technologiesList.ts
+++ configs/technologiesList.ts
@@ -1,6 +1,6 @@
 type LevelsTypes = 'basic' | 'intermedium' | 'advanced'
 
-const technologiesList:Array<String> = ['HTML', 'CSS', 'JavaScript', 'Node', 'Express', 'React', "Next", 'Tailwind', 'TypeScript', 'Git', 'Nest', 'Docker', 'PHP', 'Jest', 'Laravel', 'MySQL', 'PostgreSQL', 'MariaDB', 'OracleDB', 'MongoDB', 'React Native', 'AWS', 'Prisma', "CI/CD", "Heroku"]
+const technologiesList:Array<String> = ['HTML', 'CSS', 'JavaScript', 'Node', 'Express', 'React', "Next", 'Tailwind', 'TypeScript', 'Git', 'Nest', 'Docker', 'PHP', 'Jest', 'Laravel', 'MySQL', 'PostgreSQL', 'MariaDB', 'OracleDB', 'MongoDB', 'React Native', 'AWS', 'Prisma', "CI/CD", "Heroku", "GCP"]
 
 export default technologiesList
 export type { LevelsTypes }
