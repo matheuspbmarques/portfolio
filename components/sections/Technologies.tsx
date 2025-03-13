3812a07bb1de08d2e51a6cb765cc181525430753
Matheus Marques <matheuspbmarques@protonmail.com>
2025-03-13 16:00:41 -0300

    change: technologies

--- components/sections/Technologies.tsx
+++ components/sections/Technologies.tsx
@@ -20,16 +20,7 @@ export default function Technologies({ id }:TechnologiesProps){
         return(
             <li key={i} className={`custom-gradient p-[2px] rounded-lg flex cursor-pointer`}>
                 <div className={`h-full flex-1 bg-slate-900 rounded-[6px] ${fonts.kanitText.className} text-slate-100 flex justify-between p-2`}>
-                    <span>{ technology.name }</span>
-                    <div>
-                        <span>{
-                            technology.level == 'basic'
-                            ? 'Básico'
-                            : technology.level == 'intermedium'
-                                ? 'Intermediário'
-                                : 'Avançado'
-                        }</span>
-                    </div>
+                    <span>{ technology }</span>
                 </div>
             </li>
         )
@@ -39,7 +30,7 @@ export default function Technologies({ id }:TechnologiesProps){
         <section id={id} className="max-w-3xl flex flex-col mx-auto pt-8">
             <h2 className={`${fonts.kanitSmallTitle.className} text-2xl text-slate-100 px-6`}>Tecnologias</h2>
             <ul
-                className="flex flex-col gap-2 overflow-hidden px-6 pt-4 pb-8 sm:grid sm:grid-cols-2"
+                className="flex flex-col gap-2 overflow-hidden px-6 pt-4 pb-8 sm:grid sm:grid-cols-3"
             >
                 { renderTechnologies }
             </ul>
