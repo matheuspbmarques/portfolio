9c1bf6c993e78ac111986cd459aa4bd26f4afd6d
Matheus Marques <matheuspbmarques@protonmail.com>
2025-12-23 19:47:27 -0300

    ui: font changed from inter to Bai Jamjuree

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,37 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
+import type { Metadata } from "next";
+import { Bai_Jamjuree } from "next/font/google";
+import "./globals.css";
 
-const inter = Inter({ subsets: ['latin'] })
+const baiJamjuree200 = Bai_Jamjuree({ weight: "200", subsets: ["latin"] });
+const baiJamjuree300 = Bai_Jamjuree({ weight: "300", subsets: ["latin"] });
+const baiJamjuree400 = Bai_Jamjuree({ weight: "400", subsets: ["latin"] });
+const baiJamjuree500 = Bai_Jamjuree({ weight: "500", subsets: ["latin"] });
+const baiJamjuree600 = Bai_Jamjuree({ weight: "600", subsets: ["latin"] });
+const baiJamjuree700 = Bai_Jamjuree({ weight: "700", subsets: ["latin"] });
+
+const baiJamjuree = {
+  200: baiJamjuree200.className,
+  300: baiJamjuree300.className,
+  400: baiJamjuree400.className,
+  500: baiJamjuree500.className,
+  600: baiJamjuree600.className,
+  700: baiJamjuree700.className,
+};
 
 export const metadata: Metadata = {
-  title: 'Matheus Marques - Portfólio',
-  description: 'Este é o portfólio do Matheus Marques, um desenvolvedor FullStack',
-}
+  title: "Matheus Marques - Portfólio",
+  description:
+    "Este é o portfólio do Matheus Marques, um desenvolvedor FullStack",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="pt-br">
-      <body className={inter.className}>{children}</body>
+      <body className={baiJamjuree[400]}>{children}</body>
     </html>
-  )
+  );
 }
