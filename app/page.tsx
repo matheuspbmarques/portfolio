b3c36a20c005b94ac525c65191e72ab83aefad93
Matheus Marques <matheuspbmarques@protonmail.com>
2025-12-23 20:11:47 -0300

    ui: main page body styled

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import Header from "@/app/_components/Header"
 
 export default function Home(){
   return (
-    <body>
+    <body className={`bg-primary text-foreground`}>
       <Header />
     </body>
   )
