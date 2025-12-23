e55f682171b61d0f7d27b1c9f30c0c891e19d85e
Matheus Marques <matheuspbmarques@protonmail.com>
2025-12-23 20:09:54 -0300

    ui: sections render removed

--- app/page.tsx
+++ app/page.tsx
@@ -3,23 +3,10 @@
 //Components
 import Header from "@/app/_components/Header"
 
-//Configs
-import sectionsList from "@/configs/sectionsList"
-
 export default function Home(){
-
-  const renderSections = sectionsList.map((section, i) => {
-    if(section.sectionComponent!){
-      return(
-        <section.sectionComponent key={i} id={section.sectionId} />
-      )
-    }
-  })
-
   return (
     <body>
       <Header />
-      { renderSections }
     </body>
   )
 }
