c6b6d9c9f69abd8090d29ea3b707e4d963a14426
Matheus Marques <matheuspbmarques@protonmail.com>
2025-12-23 20:22:24 -0300

    chore: prettier apply

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,14 @@
-'use client'
+"use client";
 
 //Components
-import Header from "@/app/_components/Header"
-import { Main } from "./_components/Main"
+import Header from "@/app/_components/Header";
+import { Main } from "./_components/Main";
 
-export default function Home(){
+export default function Home() {
   return (
     <body className={`bg-primary text-foreground`}>
       <Header />
       <Main />
     </body>
-  )
+  );
 }
