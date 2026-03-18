9fd5766d5c5c931e31fe73032a32ca921b4085f4
Matheus Marques <matheuspbmarques@protonmail.com>
2026-03-17 23:13:30 -0300

    chore: upgrade tailwind to v4

--- components/ui/CustomInput.tsx
+++ components/ui/CustomInput.tsx
@@ -13,7 +13,7 @@ export default function CustomInput({
         <div className="flex flex-col gap-1">
             { label && <label htmlFor={ id }>{ label }</label>}
             <div className={`custom-gradient rounded-lg p-[2px]`}>
-                <input type={ type } className={`rounded-[6px] p-2 text-slate-100 placeholder-slate-400 bg-slate-600 w-full outline-none`} id={ id }  placeholder={ placeholder } onChange={onChange}/>
+                <input type={ type } className={`rounded-[6px] p-2 text-slate-100 placeholder-slate-400 bg-slate-600 w-full outline-hidden`} id={ id }  placeholder={ placeholder } onChange={onChange}/>
             </div>
         </div>
     )
