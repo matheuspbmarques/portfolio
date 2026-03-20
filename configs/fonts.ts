4daa3c4b2832a693b683d051d770d90a168dded4
Matheus Marques <matheuspbmarques@protonmail.com>
2026-03-20 01:32:20 -0300

    ui: change text weight

--- configs/fonts.ts
+++ configs/fonts.ts
@@ -1,7 +1,7 @@
 import { Kanit } from "next/font/google";
 
-const kanitText = Kanit({ weight: "400", subsets: ["latin"] });
-const kanitSmallTitle = Kanit({ weight: "700", subsets: ["latin"] });
+const kanitText = Kanit({ weight: "200", subsets: ["latin"] });
+const kanitSmallTitle = Kanit({ weight: "400", subsets: ["latin"] });
 
 const fonts = {
   kanitText,
