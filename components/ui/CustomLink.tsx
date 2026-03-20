6ab70173508b97aafba29f77e0d9f15d00f87c63
Matheus Marques <matheuspbmarques@protonmail.com>
2026-03-19 22:10:39 -0300

    ref: change the way as navigate between sections

--- components/ui/CustomLink.tsx
+++ components/ui/CustomLink.tsx
@@ -1,4 +1,5 @@
-import Link from "next/link";
+"use client";
+
 import fonts from "@/configs/fonts";
 
 interface CustomLinkProps {
@@ -9,12 +10,24 @@ interface CustomLinkProps {
 
 export default function CustomLink({ text, link }: CustomLinkProps) {
   return (
-    <Link
-      href={`#${link}`}
+    <button
       className={`${fonts.kanitText.className} text-sm text-slate-100 p-2 flex flex-col group`}
+      onClick={() => {
+        if (link) {
+          const sectionTop = Number(
+            document.getElementById(link)?.getBoundingClientRect().top,
+          );
+
+          const scrollTo = sectionTop < 0 ? window.scrollY - Math.abs(sectionTop) : window.scrollY + Math.abs(sectionTop);
+
+          window.scrollTo({
+            top: scrollTo - 86
+          });
+        }
+      }}
     >
       <span>{text}</span>
       <span className="w-0 h-0.5 bg-slate-100 duration-300 group-hover:last:w-full shadow-[0_0_25px_#06B6D4] bg-linear-90 from-blue-default to-purple-default" />
-    </Link>
+    </button>
   );
 }
