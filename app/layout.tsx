d3a55383f37f3f140151bc3a0c855a345fecff6d
Matheus Marques <matheuspbmarques@protonmail.com>
2026-03-21 13:24:55 -0300

    ref: metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,47 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { ME } from "@/constants";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Matheus Marques - Portfólio",
+  title: "Matheus Marques",
   description:
-    "Este é o portfólio do Matheus Marques, um desenvolvedor FullStack",
+    "Desenvolvedor FullStack autodidata com mais de 4 anos de experiência",
+  authors: [
+    {
+      name: "Matheus Marques",
+      url: ME.LINKEDIN,
+    },
+  ],
+  creator: "Matheus Marques",
+  keywords: [
+    "Desenvolvedor",
+    "Developer",
+    "FullStack",
+    "Matheus",
+    "Marques",
+    "Web",
+    "Pará",
+    "Brasil",
+    "Brazil",
+    "Norte",
+    "North",
+    "Autodidata",
+    "Software",
+    "ReactJS",
+    "React",
+    "NextJS",
+    "Next",
+    "NodeJS",
+    "Python",
+    "Node",
+    "Express",
+    "AdonisJS",
+    "PHP",
+    "Laravel",
+  ],
 };
 
 export default function RootLayout({
